64171b51aeca83a77a05390ec105baaf4d824ae8
YuanShenjian <sjyuan@thoughtworks.com>
2017-09-07 09:45:38 +0800

    Add api role

--- src/main/java/cc/sjyuan/spring/jwt/configuration/security/APISecureRolePrivilege.java
+++ src/main/java/cc/sjyuan/spring/jwt/configuration/security/APISecureRolePrivilege.java
@@ -4,5 +4,7 @@ public final class APISecureRolePrivilege {
 
     public static final String CREATE_USER = "ROLE_CREATE_USER";
     public static final String RETRIVE_USER = "ROLE_RETRIVE_USER";
+    public static final String UPDATE_USER = "ROLE_UPDATE_USER";
+    public static final String DELETE_USER = "ROLE_DELETE_USER";
 
 }
