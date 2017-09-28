29c94b096197be2ce4b0090e05512f19e75b42e6
YuanShenjian <sjyuan@thoughtworks.com>
2017-09-28 09:28:18 +0800

    Remove role and privilege repo and service

--- src/main/java/cc/sjyuan/spring/jwt/repository/PrivilegeRepository.java
+++
@@ -1,8 +0,0 @@
-package cc.sjyuan.spring.jwt.repository;
-
-
-import cc.sjyuan.spring.jwt.entity.Privilege;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PrivilegeRepository extends JpaRepository<Privilege, String> {
-}
--- src/main/java/cc/sjyuan/spring/jwt/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package cc.sjyuan.spring.jwt.repository;
-
-import cc.sjyuan.spring.jwt.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, String> {
-}
